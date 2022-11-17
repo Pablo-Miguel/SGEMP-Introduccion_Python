a2f97c18b13a38a423bca41f155f33164f4ec4b3
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-11-17 22:50:37 +0100

    V2

---
+++ Ejercicios_Boletin_3/ejercicio3.py
@@ -0,0 +1,14 @@
+print("COMPROBADOR DE RIMAS");
+
+print("Introduzca la primera palabra:")
+cadena1: str = input()
+
+print("Introduzca la segunda palabra:")
+cadena2:str = input()
+
+if cadena1.endswith(cadena2[len(cadena2) - 3:]):
+    print(f"La palabra {cadena1} rima con {cadena2}")
+elif cadena1.endswith(cadena2[len(cadena2) - 2:]):
+    print(f"La palabra {cadena1} rima un poco con {cadena2}")
+else:
+    print(f"Las palabras {cadena1} y {cadena2} no riman")
---
+++ Ejercicios_Boletin_3/ejercicio4.py
@@ -0,0 +1,11 @@
+print("Introduzca una cantidad en dólares:")
+dolares:float = float(input())
+
+print("Introduzca una tasa de interés:")
+tasa_interes:float = float(input())
+
+print("Introduzca los años:")
+anos:int = int(input())
+
+resultado:float = (dolares * ((1 + (tasa_interes/100)) ** anos))
+print(f"Una cantidad de {round(dolares, 2)}$ al {round(tasa_interes, 2)}% de interés anual se convierte en {round(resultado, 2)}$ al cabo de {anos} años")
